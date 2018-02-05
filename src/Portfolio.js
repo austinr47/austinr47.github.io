b244b90886b0a5886e3cadbb68fd352eb95f675e
Austin Randall <randall@Randall.local>
2018-02-05 15:27:16 -0700

    scolling added

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,102 +1,157 @@
 import React, { Component } from 'react';
 import './App.css';
 import scrollToComponent from 'react-scroll-to-component';
+// import { Link } from 'react-router-dom';
+import Waypoint from 'react-waypoint';
 
 class App extends Component {
+    constructor() {
+        super() 
+        this.state = {
+            about: false,
+            skills: false,
+            projects: false,
+            contact: false,
+        }
+        this.showSection=this.showSection.bind(this)
+        this.hideSection=this.hideSection.bind(this)
+    }
+
+    showSection(value) {
+        if(value === 'about') {
+            this.setState({
+                about: true,
+                skills: false,
+                projects: false,
+                contact: false,
+            })
+        } else if(value === 'projects') {
+            this.setState({
+                about: false,
+                skills: false,
+                projects: true,
+                contact: false,
+            })
+        }else if(value === 'skills') {
+            this.setState({
+                about: false,
+                skills: true,
+                projects: false,
+                contact: false,
+            })
+        }
+    }
+
+    hideSection(value) {
+        if(value === 'about') {
+            this.setState({
+                about: false,
+                skills: true,
+                projects: false,
+                contact: false,
+            })
+        } 
+    }
+
+
   render() {
+
     return (
       <div className="home-main">
         <div id="about" className='home-left'>
-          <section className='about' ref={(section) => { this.About = section; }}>
-            <div className="home-header">
-                <img alt='header' src='http://maxpixel.freegreatpicture.com/static/photo/2x/Startup-Creative-Computer-Notebooks-Start-up-593327.jpg'/>
-            </div>
-            <div className="home-about">
-                <div className='home-about-title'>
-                About Austin
-                </div>
-                <p className='home-about-content'>
-                I am a determined and hard-working developer who loves coding because the learning never ends. I approach my work with a visceral desire and unwavering determination to help my team succeed. By doing so, I find great satisfaction in the process of failing forward together.
-                </p>
-            </div>
-          </section>
-          <div id="skills" className="home-skills">
-            <section className='about' ref={(section) => { this.Skills = section; }}>
-                <div className='home-skills-title'>Skills
-                </div>
-                <p className='home-skills-paragraph'>
-                As a graduate of DevMountain, I have demonstrated proficiency with the following skills through assessments and through creating both personal and group projects using the following technologies:
-                </p>
-                <div className='home-skills-list'>
-                    <div>
-                        <ul>React | Redux</ul>
-                        <ul>JavaScript (ES6)</ul>
-                        <ul>NodeJS</ul>
-                        <ul>Express</ul>
-                        <ul>PostgreSQL</ul>
-                    </div>
-                    <div>
-                        <ul>RESTful APIs</ul>
-                        <ul>HTML5 / CSS</ul>
-                        <ul>NPM</ul>
-                        <ul>Massive</ul>
-                        <ul>Git | GitHub</ul>
-                    </div>
-                </div>
-            </section>
-          </div>
-          <div id="projects" className="home-projects">
-            <section className='about' ref={(section) => { this.Projects = section; }}>
-                <div className='home-projects-title'>
-                Projects
-                </div>
-                <div className="home-projects-content-main">
-                    <div className='home-projects-content'>
-                    <div className='home-projects-content-1'>
-                        <div className='home-projects-picture'>
-                        <img src='https://raw.githubusercontent.com/austinr47/main-personal-project/master/src/assets/account.png' alt='example'/>
+                <section className='about' ref={(section) => { this.About = section; }}>
+                        <div className="home-header">
+                            <img alt='header' src='http://maxpixel.freegreatpicture.com/static/photo/2x/Startup-Creative-Computer-Notebooks-Start-up-593327.jpg'/>
                         </div>
-                        <div className='home-projects-description'>
-                        <div className='home-projects-description-title'>FlashStudy Web Application</div>
-                        <div className='home-projects-description-awarded'>★ Awarded: "Best solution for a business" Personal Project</div>
-                        <div className='home-projects-description-from'>(DevMountain - January 2018)</div>
-                        <div className='home-projects-description-text'>Function, full-stack, responsive website built in two weeks using React with Redux, JavaScript, CSS, NodeJS, PostgreSQL, ChartJS, Digital Ocean for hosting, and more.</div>
+                        <div className="home-about">
+                            <div className='home-about-title'>
+                            About Austin
+                            </div>
+                            <Waypoint onEnter={() => {this.showSection('about')}} onLeave={() => {this.hideSection('about')}}/>
+                            <p className='home-about-content'>
+                            I am a determined and hard-working developer who loves coding because the learning never ends. I approach my work with a visceral desire and unwavering determination to help my team succeed. By doing so, I find great satisfaction in the process of failing forward together.
+                            </p>
                         </div>
-                    </div>
-                    </div>
-                    <div className='home-projects-content'>
-                    <div className='home-projects-content-1'>
-                        <div className='home-projects-picture'>
-                        <img src='https://raw.githubusercontent.com/austinr47/main-personal-project/master/src/assets/account.png' alt='example'/>
-                        </div>
-                        <div className='home-projects-description'>
-                        <div className='home-projects-description-title'>FlashStudy Web Application</div>
-                        <div className='home-projects-description-awarded'>★ Awarded: "Best solution for a business" Personal Project</div>
-                        <div className='home-projects-description-from'>(DevMountain - January 2018)</div>
-                        <div className='home-projects-description-text'>Function, full-stack, responsive website built in two weeks using React with Redux, JavaScript, CSS, NodeJS, PostgreSQL, ChartJS, Digital Ocean for hosting, and more.</div>
-                        </div>
-                    </div>
-                    </div>
-                    <div className='home-projects-content'>
-                    <div className='home-projects-content-1'>
-                        <div className='home-projects-picture'>
-                        <img src='https://raw.githubusercontent.com/austinr47/main-personal-project/master/src/assets/account.png' alt='example'/>
-                        </div>
-                        <div className='home-projects-description'>
-                        <div className='home-projects-description-title'>FlashStudy Web Application</div>
-                        <div className='home-projects-description-awarded'>★ Awarded: "Best solution for a business" Personal Project</div>
-                        <div className='home-projects-description-from'>(DevMountain - January 2018)</div>
-                        <div className='home-projects-description-text'>Function, full-stack, responsive website built in two weeks using React with Redux, JavaScript, CSS, NodeJS, PostgreSQL, ChartJS, Digital Ocean for hosting, and more.</div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
                 </section>
-                <section className='about' ref={(section) => { this.Contact = section; }}>
-            <div id="contact" className="home-contact">
+            <div id="skills" className="home-skills">
+                    <section className='about' ref={(section) => { this.Skills = section; }}>
+                        <div className='home-skills-title'>Skills
+                        </div>
+                        <p className='home-skills-paragraph'>
+                        As a graduate of DevMountain, I have demonstrated proficiency with the following skills through assessments and through creating both personal and group projects using the following technologies:
+                        </p>
+                        <div className='home-skills-list'>
+                            <div>
+                                <ul>React | Redux</ul>
+                                <ul>JavaScript (ES6)</ul>
+                                <ul>NodeJS</ul>
+                                <ul>Express</ul>
+                                <ul>PostgreSQL</ul>
+                            </div>
+                            <div>
+                                <ul>RESTful APIs</ul>
+                                <ul>HTML5 / CSS</ul>
+                                <ul>NPM</ul>
+                                <ul>Massive</ul>
+                                <ul>Git | GitHub</ul>
+                            </div>
+                        </div>
+                        <Waypoint onEnter={() => {this.showSection('skills')}} onLeave={() => {this.hideSection('a')}}/>
+                    </section>
             </div>
-            </section>
-          </div>
+            <div id="projects" className="home-projects">
+                    <section className='about' ref={(section) => { this.Projects = section; }}>
+                        <div className='home-projects-title'>
+                        Projects
+                        </div>
+                        <div className="home-projects-content-main">
+                            <div className='home-projects-content'>
+                            <div className='home-projects-content-1'>
+                                <div className='home-projects-picture'>
+                                <a href='http://myflash-study.com/#/' rel="noopener noreferrer" target="_blank" className='home-projects-picture'><img src='https://raw.githubusercontent.com/austinr47/main-personal-project/master/src/assets/account.png' alt='example'/></a>
+                                </div>
+                                <div className='home-projects-description'>
+                                <div className='home-projects-description-title'>FlashStudy Web Application</div>
+                                <div className='home-projects-description-awarded'>★ Awarded: "Best solution for a business" Personal Project</div>
+                                <div className='home-projects-description-from'>(DevMountain - January 2018)</div>
+                                <div className='home-projects-description-text'>Function, full-stack, responsive website built in two weeks using React with Redux, JavaScript, CSS, NodeJS, PostgreSQL, ChartJS, Digital Ocean for hosting, and more.</div>
+                                </div>
+                            </div>
+                            </div>
+                            <Waypoint onEnter={() => {this.showSection('projects')}} onLeave={() => {this.hideSection('contact')}}/>
+                            <div className='home-projects-content'>
+                            <div className='home-projects-content-1'>
+                                <div className='home-projects-picture'>
+                                <img src='https://raw.githubusercontent.com/austinr47/main-personal-project/master/src/assets/account.png' alt='example'/>
+                                </div>
+                                <div className='home-projects-description'>
+                                <div className='home-projects-description-title'>Group Project</div>
+                                <div className='home-projects-description-awarded'>★</div>
+                                <div className='home-projects-description-from'>(DevMountain - Feburary 2018)</div>
+                                <div className='home-projects-description-text'>Detailed project description will go here</div>
+                                </div>
+                            </div>
+                            </div>
+                            <div className='home-projects-content'>
+                            <div className='home-projects-content-1'>
+                                <div className='home-projects-picture'>
+                                <img src='https://raw.githubusercontent.com/austinr47/main-personal-project/master/src/assets/account.png' alt='example'/>
+                                </div>
+                                <div className='home-projects-description'>
+                                <div className='home-projects-description-title'>React Mini Projects</div>
+                                <div className='home-projects-description-awarded'></div>
+                                <div className='home-projects-description-from'>(DevMountain - January 2018)</div>
+                                <div className='home-projects-description-text'>These are just a few small React exercises that I have created. Including: a calculator, a To-Do list, and a weather app. They are all mobile responsive and use various technologies.  </div>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section className='about' ref={(section) => { this.Contact = section; }}>
+                        <div id="contact" className="home-contact">
+                        </div>
+                    </section>
+            </div>
         </div>
         <div className='home-right-1'>
         </div>
@@ -107,10 +162,10 @@ class App extends Component {
           <div className='menu-name'>Austin Randall</div>
           <div className='menu-title'>Web Developer</div>
           <div className='menu-sections'>
-            <a onClick={() => scrollToComponent(this.About, { offset: 0, align: 'top', duration: 500})} className='menu-about'>About</a>
-            <a onClick={() => scrollToComponent(this.Skills, { offset: 0, align: 'top', duration: 500})} className='menu-skills'>Skills</a>
-            <a onClick={() => scrollToComponent(this.Projects, { offset: 0, align: 'top', duration: 500})} className='menu-projects'>Projects</a>
-            <a onClick={() => scrollToComponent(this.Contact, { offset: 0, align: 'top', duration: 500})} className='menu-contact'>Contact</a>
+            <a onClick={() => scrollToComponent(this.About, { offset: 0, align: 'top', duration: 750})} className={this.state.about ? 'menu-about showSection' : 'menu-about'}>About</a>
+            <a onClick={() => scrollToComponent(this.Skills, { offset: 0, align: 'top', duration: 750})} className={this.state.skills ? 'menu-about showSection' : 'menu-about'}>Skills</a>
+            <a onClick={() => scrollToComponent(this.Projects, { offset: 0, align: 'top', duration: 750})} className={this.state.projects ? 'menu-about showSection' : 'menu-about'}>Projects</a>
+            <a onClick={() => scrollToComponent(this.Contact, { offset: 0, align: 'top', duration: 750})} className={this.state.contact ? 'menu-about showSection' : 'menu-about'}>Contact</a>
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -7,4 +7,3 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 // registerServiceWorker();
-
